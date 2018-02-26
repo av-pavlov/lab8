13f08f832168b4661e0f1cee68b3003af1c11fdc
Alexander Pavlov <7911562+av-pavlov@users.noreply.github.com>
2018-02-26 09:35:44 +0900

    pochti sdelal 2

--- lab8.cpp
+++ lab8.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 
 using namespace std;
 
-int main()
+long long size, count, fcount;
+
+void dfs()
 {
-	DIR* dp = opendir(".");
-	struct dirent *res;
+    DIR* dp = opendir(".");
+	struct dirent *res;       // структура для данных записи каталога
 
 	while ((res = readdir(dp)) != NULL) {
-    	cout << res->d_name << "\n";
+	    if (strcmp(res->d_name, ".")==0 || strcmp(res->d_name, "..")==0)
+            continue;
+        struct stat info;    // структура для данных записи stat
+        stat(res->d_name, &info); // получаем данные в структуру info
+        if (S_ISDIR(info.st_mode)) {  // если это каталог...
+            cout << "DIR ";
+            fcount++;
+        } else {
+            size += info.st_size;
+            count++;
+        }
 	}
     closedir(dp);
+}
 
+int main()
+{
+	dfs();
+	cout << count << " files, " << fcount << " dirs, " << size << " bytes.\n";
     return 0;
 }
 
